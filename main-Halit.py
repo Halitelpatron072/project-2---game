75d7773b68f0afad044041e546cedff7524e0804
Halit <183967@student.talland.nl>
2025-01-29 11:49:57 +0100

    feat: "Chiggacoins gevoegd en je kan meer verdienen"

--- main-Halit.py
+++ main-Halit.py
@@ -1,13 +1,24 @@
 import sys
 from time import sleep
 
+chiggacoins = 0
+
 # Functie om tekst te printen in een coole stijl
 def typewrite(str):
     for karakter in str:
-        sleep(0.02)
+        sleep(0.015)
         sys.stdout.write(karakter)
         sys.stdout.flush()
     sys.stdout.write('\n')
+    
+def addcoin(coins):
+    global chiggacoins
+    chiggacoins += coins
+    typewrite(f"Je hebt {coins} chiggacoin(s) gekregen. Je hebt in totaal nu {chiggacoins} chiggacoin(s)\n\n")
+
+def eind():
+    typewrite(f"Je hebt de spel beindigd met {chiggacoins} chiggacoin(s)")
+    exit()
 
 # Print het logo uit
 print("""
@@ -37,12 +48,13 @@ while True:
         typewrite("voordat je begint, wil je fent?\n\nja\n\nnee")
         keuze = input("> ")
         
+        
         #random keuze (keuze 1)
         if keuze == "nee":
-            typewrite("isg")
-            exit()
+            typewrite("isg\n\n")
+            eind()
         elif keuze == "ja":
-            typewrite("ai")
+            typewrite("ai\n\n")
             break
         else:
             typewrite("Ongeldige operatie")
@@ -56,7 +68,7 @@ while True:
 # Keuze 2(spel keuze 1)
 while True:
     if keuze == "ja":
-        typewrite("Je zit vast in de gevangenis van Chopped Chin,\n maar jij wilt hier zo snel mogelijk weg.\n Dus jij gaat je best proberen om te ontsnappen.\n")
+        typewrite("Je zit vast in de gevangenis van Chopped Chin,\nmaar jij wilt hier zo snel mogelijk weg.\nDus jij gaat je best proberen om te ontsnappen.\n")
         typewrite("Wat wil je doen?\n")
         typewrite("1. Al je laatjes doorzoeken onder je bed\n\n2. Bewaker misleiden")
         keuze2 = input("> ")
@@ -64,13 +76,14 @@ while True:
         #keuze die leidt naar keuze3(spel keuze 2)
         if keuze2 == "1":
             typewrite("Je hebt opeens uit het niets allerlei soorten schroevendraaiers\ngevonden en je kijkt om je heen en je ziet een ventilatierooster boven\nje bed en je maakt het open en je gaat erin,\ngoed gedaan " + naam + "\n\n")
-            input("Press ENTER to continue")
+            addcoin(1)
+            input("> Press ENTER to continue")
             break
         
         #keuze die leidt naar einde(Slechte leugenaar)
         elif keuze2 == "2":
             typewrite("Je deed alsof je ziek was en de bewaker\n had je door.\n\n(Slechte leugenaar)\n\n")
-            exit()
+            eind()
            
         #ongeldige keuze(opnieuw kiezen van dit sectie)
         else:
@@ -87,11 +100,13 @@ while True:
     #einde(YamYam)
     if keuze3 == "1":
         typewrite("Je hebt de linker ventilatierooster geopend en je valt weer in een cel,\nmaar je bent niet alleen...\nnee, je bent in het cel terecht gekomen van 'El yamyam'\nJe bent dus uiteindelijk nog steeds niet ontsnapt en hij heeft je opgevreten.\n\n(Yamyam einde)\n\n")
-        exit()
+        eind()
     
     #keuze die je leidt naar keuze4(spel keuze 3)
     elif keuze3 == "2":
         typewrite("Je opent de rechter ventilatierooster en je valt in een kleedkamer en niemand is er.\nJe kijkt rond en je vind een open locker met bewaker uniform\nPRECIES jouw maat toevallig en je doet het aan en loopt uit de kleedkamer.\n\n")
+        addcoin(1)
+        input("> Press ENTER to continue ")
         break
     
     #ongeldige keuze(opnieuw kiezen van dit sectie)
@@ -103,24 +118,26 @@ while True:
 while True:
     typewrite("Je kleed aan als een bewaker en je loopt uit de kleedkamer enzo,\nmaar toen zag je een bewaker en hij vroeg zich af wat je doet in de nacht hier doet\nen vroeg voor je id. Wat ga je doen?")
     typewrite("wat ga je doen" + naam +"?\n")
-    typewrite("1. Hem afleiden en praten met hem\n\n2. Hem in elkaar slaan\n\n3. Hem afleiden en wegrennen")
+    typewrite("1. vriendelijk zijn en over andere dingen praten\n\n2. Hem knockout slaan slaan\n\n3. Wegrennen")
     keuze4 = input("> ")
 
     #einde(Happy)
     if keuze4 == "1":
         typewrite("Hij vond je aardig en nam je mee naar een bar en jullie waren beste vrienden en niemand was gewond.\n\n(Happy einde)\n\n")
-        exit()
+        eind()
     
     #keuze die leidt naar keuze5(spel keuze 4)
     elif keuze4 == "2":
         typewrite("Je slaat hem in elkaar en je verstopt zijn\nlichaam in een donkere hoek en je pakt zijn wapen\n\n")
-        anderekant = input("Press ENTER to continue ")
+        addcoin(1)
+        input("> Press ENTER to continue ")
         break
     
     #keuze die leidt naar keuze6(spel keuze 5)
     elif keuze4 == "3":
         typewrite("Je rent weg en je ziet dat hij achter je aanrent\n\n")
-        anderekanteen = input("Press ENTER to continue ")
+        addcoin(1)
+        input("> Press ENTER to continue ")
         break
     
     #ongeldige keuze(opnieuw kiezen van dit sectie)
@@ -138,11 +155,13 @@ if keuze4 == "2":
         #keuze die leidt naar einde(Dikke pech)
         if keuze5 == "1":
             typewrite("Je gebruikt het wapen en doodt de bewaker die al onbewusteloos lag, amar de alarm ging af. Ga daar zo snel mogelijk weg!\n\n")
+            addcoin(1)
             break
         
         
         elif keuze5 == "2":
             typewrite("Je bewaart het wapen en besluit verder te gaan zonder te vechten.\n\n")
+            addcoin(1)
             break
         
         #ongeldige keuze(opnieuw kiezen van dit sectie)
@@ -153,12 +172,12 @@ if keuze4 == "2":
 #einde(Dikke pech)
 if keuze5 == "1":
     typewrite("Je rent en rent en rent en uiteindelijk de\nuitgang gevonden. Je doet de deur open helemaal blij\nen denkt wat je allemaal gaat doen als je vrij bent, maar je hebt te vroeg gejuicht,\nwant de politie is er al. JE geeft je over en je gaat de bak weer in.\n\n(Dikke pech einde)\n\n")
-    exit()
+    eind()
 
 #einde(Lijpe einde)
 if keuze5 == "2":
     typewrite("Je kiest om niet te schieten en je vind een uitgang en je loopt weg en je gaat lekker naar huis als een soldaat\n\n(Lijpe einde)\n\n")
-    exit()
+    eind()
 
 #keuze6(spel keuze 5)
 if keuze4 == "3":
@@ -170,36 +189,41 @@ if keuze4 == "3":
         #keuze die leidt naar einde(IDKsterling)
         if keuze6 == "1":
             typewrite("Je verstopt je in een kamer en hoopt dat de bewaker je niet vindt\nen je kijkt achter je en je ziet IDKsterling\nen hij zegt dat hij een uitweg weet en je gaat hem dus volgen\n\n")
-            eindigspel = input("Press ENTER to continue ")
+            addcoin(1)
+            input("> Press ENTER to continue ")
             break
         
         #einde(Gedood)
         elif keuze6 == "2":
             typewrite("Je rent naar de uitgang, maar de bewakers zijn dicht achter je en ze hebben je helemaal doodgeschoten a sahbi.\n\n(Je bent gedood einde)")
-            exit()
+            eind()
         
         #keuze die leidt naar einde(toilet)
         elif keuze6 == "3":
             typewrite("Je verstopt je un een toilet en je legt een dikke beuker\ndoor die pittige sucuk die je vanmiddag at\nen na je boodschap zie je dat er een kleine raampje zit boven de toilet.\nDe bewaker is er inmiddels al voor trapt op de deur,\ndus je veegt af en je klimt door de raam")
+            addcoin(1)
+            input("> Press ENTER to continue ")
             break
         
         #einde(ritueel)
         elif keuze6 == "4":
             typewrite("Je doet een lijpe ritueel en je roept John'Chigga'Pork op van zijn dood en hij vermoord iedereen en neemt je mee naar buiten en je bent ontsnapt.\n\n(Ritueel einde)")
-        
+            eind()
+            
         #ongeldige keuze(opnieuw kiezen van dit sectie)
         else:
-            typewrite("Ongeldige keuze\n")
+            typewrite("> Ongeldige keuze\n")
             continue
 
 #einde(IDKsterling)
 if keuze6 == "1":
     typewrite("Je ziet dat Sterling al een gat heeft gegraven\ndie leidt naar de riool en je volgt hem dus.\nJe ontsnapt uiteindelijk uit de gevangenis.\n\n(Onstnapt met IDKsterling einde)\n\n")
-    exit()
+    eind()
 
 #einde(toilet)    
 if keuze6 == "3":
     typewrite("Je hebt maar weinig tijd" + naam +". De bewaker probeert die deur open te trappen. klim nu uit het raam")
+    addcoin(1)
     input("Press ENTER to 'klim uit het raam'")
     typewrite("Je bent door de raam geklommen en je rent weg en je bent ontsnapt.\n\n(Toiletraam ontsnapping einde)\n\n")
-    exit()
+    eind()
